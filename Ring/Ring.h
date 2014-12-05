b6755e1876ce391e92cae7208f4e202269088481
Jeremy Tregunna <jtregunna@quinn.local>
2014-12-05 11:59:44 -0600

    Initial Commit

---
+++ Ring/Ring.h
@@ -0,0 +1,19 @@
+//
+//  Ring.h
+//  Ring
+//
+//  Created by Jeremy Tregunna on 2014-12-05.
+//  Copyright (c) 2014 Jeremy Tregunna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ring.
+FOUNDATION_EXPORT double RingVersionNumber;
+
+//! Project version string for Ring.
+FOUNDATION_EXPORT const unsigned char RingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ring/PublicHeader.h>
+
+
---
+++ RingTests/RingTests.swift
@@ -0,0 +1,36 @@
+//
+//  RingTests.swift
+//  RingTests
+//
+//  Created by Jeremy Tregunna on 2014-12-05.
+//  Copyright (c) 2014 Jeremy Tregunna. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
